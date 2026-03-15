94e0b7c48ef36a010e5f8a7cd709d19f56a1824a
Dylan <dylanmckay8939@gmail.com>
2026-03-15 05:35:42 -0400

    Add vercel origin to allowed origins

--- app/main.py
+++ app/main.py
@@ -25,7 +25,7 @@ app = FastAPI(title="Fading Visions API")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["http://localhost:5173", "https://fading-visions-api.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
